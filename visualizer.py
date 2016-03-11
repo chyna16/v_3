957203a160b2a8d3b57d8ec825a53ad5ec24735c
Tramain Bentinck <tramain.bentinck@viacommix.com>
2016-03-11 17:33:20 -0500

    refactor code for increased readability

--- visualizer.py
+++ visualizer.py
@@ -1,48 +1,49 @@
+import csv, os, time, fnmatch, json
 from flask import Flask, request, render_template
-import csv, os, time, fnmatch
-from werkzeug import secure_filename
-import json
 
 # this is the directory of where the html page obtains the list of files to select from
+# owd = obtain working directory
 owd = os.getcwd()
 
+# currently the repository  and codemaat directory is hardcoded
+
 if os.name == 'nt':
-	rootPath = os.path.normpath(owd + '/csv_files')
-	maatPath = os.path.normpath('C:/Users/bentinct/winmaat0.8.5/')
-	repoDir = os.path.normpath('C:/Users/bentinct/repos/mcshake/.git')
+	root_dir = os.path.normpath(owd + '/csv_files')
+	maat_dir = os.path.normpath('C:/Users/bentinct/winmaat0.8.5/')
+	repo_dir = os.path.normpath('C:/Users/bentinct/repos/mcshake/.git')
 else:
-	rootPath = owd + '/csv_files'
-	maatPath = '/home/tramain/ixmaat0.8.5'
-	repoDir = '/home/tramain/mcshake/.git'
-# rootPath = '/home/ubuntu/Desktop/repos/v3clone/v3/csv_folder'
+	root_dir = owd + '/csv_files'
+	maat_dir = '/home/tramain/ixmaat0.8.5'
+	repo_dir = '/home/tramain/mcshake/.git'
 
 # this returns only files of this type to the html page to display.
-pattern = '*.csv'
+file_type = '*.csv'
 
 app = Flask(__name__)
 
 # -------------------------------------------------------------------
-# 'git --git-dir C:\users\bentinct\repos\mcshake\.git log --pretty=format:"[%h] %aN %ad %s" --date=short --numstat > logfile.log'
-# code below only works with code maat downloaded 
+
+# code below only works with codemaat downloaded
 
 
 def set_path():
 	print("Setting a path for codemaat...")
 	# path is set temporarily, per script run
-	os.environ['PATH'] += os.pathsep + maatPath
+	os.environ['PATH'] += os.pathsep + maat_dir
 	print("Done.")
 	print("-" * 60)
 	# need to create an exception if csv_files folder already exists
 	# global owd
 	# owd = os.getcwd()
-	# creates folder for the rootPath variable if none exists
+	# creates folder for the root_dir variable if none exists
 	os.system("mkdir csv_files")
 	os.chdir("csv_files")
 
-def create():
+
+def generate_data():
 	print("Obtaining repository logs...")
 	# currently manually selects the repository
-	os.system('git --git-dir '+ repoDir +' log --pretty=format:"[%h] %aN %ad %s" --date=short --numstat > logfile.log')
+	os.system('git --git-dir ' + repo_dir + ' log --pretty=format:"[%h] %aN %ad %s" --date=short --numstat > logfile.log')
 	print("Done.")
 	print("-" * 60)
 	print("Creating csv files from generated log...")
@@ -64,56 +65,58 @@ def create():
 	# Reports how long ago the last change was made in measurement of months
 	print("Done. Check your current folder for your files.")
 	print("-" * 60)
-	# os.chdir(owd);
+	os.chdir("..")
 
-def browser():
+
+def open_browser():
 	print("Process complete. Opening browser to http://127.0.0.1:5000/")
 	time.sleep(2)
 	os.system("google-chrome-stable http://127.0.0.1:5000")
-	print("-" * 60);
+	print("-" * 60)
 
 
 # --------------------------------------------------------------------
 
 # this function takes csv file and two empty arrays
 # reads each column from file into an array and returns the arrays
-def parseCSV(uploadedFile, dataArray):
-	rowArray = []
+def parse_csv(uploaded_file, data_array):
+	row_array = []
 	length = 0
 	i = 0
-	reader = csv.reader(uploadedFile)
+	reader = csv.reader(uploaded_file)
 	for row in reader:
 		length = len(row)
-		rowArray.append(row)
-	print (rowArray[0])
+		row_array.append(row)
+	print(row_array[0])
 	while i < length:
-		colArray = []
-		for r in rowArray:
-			colArray.append(r[i])
-		dataArray.append(colArray)
-		i+=1
-	return dataArray;
+		col_array = []
+		for r in row_array:
+			col_array.append(r[i])
+		data_array.append(col_array)
+		i += 1
+	return data_array
+
 
 @app.route('/', methods=['GET', 'POST'])
-def input():
-	dataArray = []
-	
+def obtain_data():
+	data_array = []
+
 	# upon opening the homepage, user is prompted with a selection of repos
 	if request.method == 'GET':
-		csvList = []
-		for root, dirs, files in os.walk(rootPath): # traverses filesystem of rootPath
-			for filename in fnmatch.filter(files, pattern): # picks out files of type in pattern
-				csvList.append(filename) # fills array with names of csv files in current directory
-		return render_template('input.html', csvList=csvList) # returns array of csv filenames to webpage
-	
+		csv_list = []
+		for root, dirs, files in os.walk(root_dir):  # traverses filesystem of root_dir
+			for filename in fnmatch.filter(files, file_type):  # picks out files of type in pattern
+				csv_list.append(filename)  # fills array with names of csv files in current directory
+		return render_template('input.html', csvList=csv_list)  # returns array of csv filenames to webpage
+
 	# when user selects a repo, the following runs codemaat and generates a csv file
 	# the csv file is opened and parsed; visualization is displayed
 	elif request.method == 'POST':
-		data = request.form['filename'] # gets name of csv filename that was selected by the user on webpage
-		with open('{}'.format(data), 'rt') as csvfile: # variable data SHOULD be in form of 'csvname.csv'
-			parseCSV(csvfile, dataArray)
-		return render_template('result.html', dataArray=json.dumps(dataArray))
-		csvfile.close();
+		data = request.form['filename']  # gets name of csv filename that was selected by the user on webpage
+		with open('/csv_files'.format(data), 'rt') as csv_file:  # variable data SHOULD be in form of 'csvname.csv'
+			parse_csv(csv_file, data_array)
+		return render_template('result.html', dataArray=json.dumps(data_array))
+		# csv_file.close()
 
 # THIS NEEDS TO BE WORKED ON
 # TRYING TO GET ROUTE('/') TO REDIRECT TO BELOW ROUTE AFTER RENDER_TEMPLATE
@@ -122,10 +125,10 @@ def input():
 # 	return render_template('result.html')
 
 if __name__ == '__main__':
-	# debug mode causes create() function to run twice
+	# debug mode causes generate_data() function to run twice
 	# app.debug = True
 	set_path()
-	create()
-	browser()
+	generate_data()
+	open_browser()
 	app.run()
-	#app.run(host='0.0.0.0')
+	# app.run(host='0.0.0.0')
