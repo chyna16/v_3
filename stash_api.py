61857925eccf4bf36b3cf6c6d49451a87e426b42
Tramain Bentinck <Tramain.Bentinck@Viacommix.com>
2016-04-26 20:17:18 -0400

    add extra api functions

--- stash_api.py
+++ stash_api.py
@@ -4,18 +4,27 @@ from json import load
 import json
 import codecs
 
+
 url_projects = 'https://stash.mtvi.com/rest/api/1.0/projects' 
 projects = requests.get(url=url_projects, auth=('username', 'password'))
 
 # reader = codecs.getreader("utf-8")	
 json_projects = json.loads(projects.text)
 
-print(projects)
+# print(projects)
 
-# def get_details():
+# def get_project_url():
 # 	for project in json_projects['values']:
-# 		for details in project:
-# 			print (project[details])
+# 		for details in project['links']['self']:
+# 			print (details['href'])
+
+
+def get_project_names():
+	global project_list
+	project_list = []
+	for projects in json_projects['values']:
+		project_list.append(projects['name'])
+
 
 def get_details():
 	for project in json_projects['values']:
@@ -25,7 +34,6 @@ def get_details():
 			print ("Description: " + project['description'])
 		print ("-" * 60)
 
-get_details()
 
 def get_clone_url():
 	for project in json_projects['values']:
@@ -40,3 +48,10 @@ def get_clone_url():
 			for link in repo['links']['clone']:
 				if link['name'] == "http":
 					print (link['href'])
+
+get_project_names()
+
+if __name__ == '__main__':
+	get_project_names()
+	print (project_list)
+	get_details()
