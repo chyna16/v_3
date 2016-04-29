e474c2c6d10cdf513a884b1cc1eb6d8b9a6a72f7
Tramain Bentinck <Tramain.Bentinck@Viacommix.com>
2016-04-29 02:10:04 -0400

    add new views to test api functionality

--- visualizer.py
+++ visualizer.py
@@ -2,6 +2,7 @@ import os
 import fnmatch
 import json
 import generator
+# import stash_api
 from flask import Flask, request, render_template, redirect, url_for, flash
 
 # owd = obtain working directory
@@ -38,9 +39,8 @@ def index_project():
 	generator.clone_url = ""
 	generator.password = ""
 	generator.repo_name = ""
+	project_select = generator.project_key
 	if request.method == 'GET':
-		project_select = generator.project_list
-		print(project_select)
 		return render_template('index_project.html', project_select=project_select)  # returns array of csv filenames to webpage
 	elif request.method == 'POST' and not request.form['clone_url'] == "":
 			generator.clone_url = request.form['clone_url']
@@ -48,9 +48,34 @@ def index_project():
 			generator.submit_url()
 			flash('Cloning complete.')
 			return redirect(url_for('index_project'))
-	elif request.method == 'POST' and not request.form['folder_name'] == "": 
-			select_folder()
-			return redirect(url_for('dashboard'))
+	elif request.method == 'POST' and not request.form['project_name'] == "":
+		generator.selected_key = (request.form['project_name'])
+		print(request.form['project_name'])
+		for key in project_select:
+			if request.form['project_name'] == key:
+				generator.get_project_key(key)
+		return redirect(url_for('index_repo'))
+			# select_folder()
+			# print(request.form['project_name'])
+			# return redirect(url_for('dashboard'))
+
+
+@app.route('/index_repo', methods=['GET', 'POST'])
+def index_repo():
+	generator.clone_url = ""
+	generator.password = ""
+	generator.repo_name = ""
+	if request.method == 'GET':
+		return render_template('index_repo.html', repo_select=generator.project_repo_name, repo_url=generator.project_repo_list)  # returns array of csv filenames to webpage
+	elif request.method == 'POST' and not request.form['clone_url'] == "":
+			generator.clone_url = request.form['clone_url']
+			generator.password = request.form['password']
+			generator.submit_url()
+			flash('Cloning Complete.')
+			return redirect(url_for('index'))
+	elif request.method == 'POST' and not request.form['repo_name'] == "":
+			# temporary redirect
+			return redirect(url_for('index'))
 
 
 @app.route('/dashboard', methods=['GET', 'POST'])
@@ -99,6 +124,31 @@ def bad_request(e):
 
 
 def select_folder():
+	global root_dir
+	print (request.form['folder_name'])
+	generator.repo_name = request.form['folder_name']  # gets name of repository that was selected by the user on webpage
+	# print (generator.repo_name)
+	generator.set_path(generator.repo_name)
+	generator.date_after = request.form['date_after']
+	generator.date_before = request.form['date_before']
+	# print (generator.date_after)
+	# print (generator.date_before)
+	root_dir = (owd + '/csv_files_' + generator.repo_name + "_" 
+		+ generator.date_after + "_" + generator.date_before)
+	# if directory already exists, skip function and go to the next page
+	if(os.path.exists(generator.repo_list + "v3/csv_files_" 
+		+ generator.repo_name + "_" + generator.date_after + "_" 
+		+ generator.date_before)): 
+		print("folder exists:" + generator.repo_list + "v3/csv_files_" 
+			+ generator.repo_name + "_" + generator.date_after + "_" 
+			+ generator.date_before)
+		flash('Directory exists, redirected to current page.')
+		return redirect(url_for('dashboard'))
+	else:
+		generator.generate_data(address = generator.repo_list + generator.repo_name + '/.git')
+		flash('Analysis complete.')
+
+def select_repo():
 	global root_dir
 	print (request.form['folder_name'])
 	generator.repo_name = request.form['folder_name']  # gets name of repository that was selected by the user on webpage
