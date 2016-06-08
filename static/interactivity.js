b98e980f8e48302faec322a86c17763fb8e3aec3
Farhat Nader <farhat.nader01@myhunter.cuny.edu>
2016-06-08 14:11:40 -0400

    added comments; modified bits of code

--- static/interactivity.js
+++ static/interactivity.js
@@ -1,19 +1,4 @@
-// called when user selects a column to filter by, and selects range
-// reads user entries and sets variables accordingly
-// if one of start/end is left empty, assigns default value
-// calls createTable
-function setFilter() {
-    filter_key = document.forms["filter"]["key"].value;
-    filter_start = parseInt(document.forms["filter"]["start"].value);
-        if (filter_start == null || filter_start == "") { filter_start = 0; }
-    filter_end = parseInt(document.forms["filter"]["end"].value);
-        if (filter_end == null || filter_end == "") { filter_end = 100000; }
-
-    for (i=0; i<keys_len; i++) { if (filter_key == keys[i]) { filterData(); } }
-}
-
-
-// called by createTable
+// called by filterData
 // checks to see if the passed in value falls within the range of the filter
 // returns true if filter should be applied on said value, false if not
 function applyFilter(filter_value) {
@@ -27,52 +12,74 @@ function applyFilter(filter_value) {
 }
 
 
+// called by setFilter
+// reassigns filtered_data to new set of data meeting filter requirements
+function filterData() {
+    filtered_data = []; // filtered_data is emptied, and updated below
+
+    json_data.forEach(function (d) {
+        // if the current value of the filter column chosen by the user
+        // falls within the filter range, then the loop skips the following
+        if (applyFilter(+d[filter_key]) != true) {
+            filtered_data.push(d);
+        }
+    });
+
+    createTable(filtered_data);
+}
+
+
+// called when user selects a column to filter by, and selects range
+// reads user entries and sets variables accordingly
+// if one of start/end is left empty, assigns default value
+function setFilter() {
+    filter_key = document.forms["filter"]["key"].value;
+    filter_start = parseInt(document.forms["filter"]["start"].value);
+        if (filter_start == null || filter_start == "") { filter_start = 0; }
+    filter_end = parseInt(document.forms["filter"]["end"].value);
+        if (filter_end == null || filter_end == "") { filter_end = 100000; }
+
+    for (i = 0; i < keys.length; i++) { if (filter_key == keys[i]) { filterData(); } }
+}
+
+
+// called when user selects an entity in coupling analysis
+// parses data for every object in which that entity is present
+// creates new data with those objects
 function chooseModule(elem) {
     var chosen_module = elem.getAttribute('value');
 
     coupled_data = [];
 
-    filtered_json.forEach(function(d) {
+    filtered_data.forEach(function(d) {
         if (d[keys[0]] == chosen_module || d[keys[1]] == chosen_module) {
+            // if chosen entity is found in either first / second column of row
             var temp_obj = {};
             keys.forEach(function(k, i) {
+                // traverse row and create object w/ all data aside from entity
                 if (d[k] != chosen_module) {
                     if (k == 'entity') { temp_obj.coupled = d[k]; }
                     else { temp_obj[k] = d[k]; }
                 }
             })
-            console.log(temp_obj);
-            coupled_data.push(temp_obj);
+            coupled_data.push(temp_obj); // push the object into new data
         }
     });
 
-    createMeter(coupled_data, chosen_module);
+    createMeter(coupled_data, chosen_module); // meter chart is created
 }
 
 
-function filterData() {
-    filtered_json = [];
-
-    json_data.forEach(function (d) {
-        // if the current value of the filter column chosen by theuser
-        // falls within the filter range, then the loop skips the following
-        if (applyFilter(+d[filter_key]) != true) {
-            filtered_json.push(d);
-        }
-    });
-
-    createTable(filtered_json);
-}
-
-
+// called when user clicks on column header
 // specifies the y-axis values for the graphs to whichever column the user selects
 // calls function to create graph using the selected data
 function chooseColumn(elem) {
-    chosen_key = elem.getAttribute('value');
-    createGraph(filtered_json); 
+    chosen_key = elem.getAttribute('value'); // chosen_key is reassigned
+    createGraph(filtered_data); 
 }
 
 
+// NOTE: currently not in use
 // changes the display of corresponding graph to show or hide
 function toggleGraph(elem) {
     var display_status = document.getElementById(elem.getAttribute('name')).style.display;
--- static/table.js
+++ static/table.js
@@ -1,34 +1,46 @@
+// called by createTable
+// takes a row element, a column id, and a row of data
+// fills row element w/ a column for each item in the row of data
+// returns a completed row
 function setTableRows(row, elem_id, data) {
     for (i = 0; i < keys.length; i++) {
-        var col = document.createElement(elem_id);
+        var col = document.createElement(elem_id); // a th / td element
 
-        if (i == 0) { col.style.width = "40%"; }
-        else { col.style.width = (60 / (keys_len-1)) + "%"; }
+        if (i == 0) { col.style.width = "40%"; } // first column width
+        else { col.style.width = (60 / (keys.length - 1)) + "%"; } 
+            // remaining column widths
 
+        // column styling
         col.style.overflow = "hidden";
         col.style.fontSize = "15px";
         col.onmouseover = function() { this.style.overflow = "visible"; }
         col.onmouseout = function() { this.style.overflow = "hidden"; }
-        // col.style.backgroundColor = "white"; INTERFERES W/ OVERFLOW
+        // col.style.backgroundColor = "white"; // INTERFERES W/ OVERFLOW
 
         if (data == keys) {
-            if (i != 0 && keys[i] != "coupled") { 
+            // if table head is being created
+            if (i != 0 && keys[i] != "coupled") {
+                // if column is has a numerical value
                 col.onclick = function() { chooseColumn(this); }
+                    // allow user to click to specify column
                 col.style.cursor = 'pointer';
             }
             col.setAttribute('value', data[i]);
             col.appendChild(document.createTextNode(data[i])); 
         } 
         else { 
+            // if table body is being created
             if ((i == 0 || i == 1) && analysis_type == "coupling") { 
+                // if coupling analysis and col is 'entity' or 'coupled'
                 col.onclick = function() { chooseModule(this); }
+                    // allow user to click to specify entity for coupling
                 col.style.cursor = 'pointer';
             }
             col.setAttribute('value', data[keys[i]]);
             col.appendChild(document.createTextNode(data[keys[i]])); 
         }
 
-        row.appendChild(col);
+        row.appendChild(col); // each column created is added to passed in row
     }
     return row;
 }
@@ -36,15 +48,18 @@ function setTableRows(row, elem_id, data) {
 
 // parses data from dictionary using keys and fills each row.col with values
 function createTable(data) {
-    d3.select("#thead").html(""); // table is emptied before being filled
-    d3.select("#tbody").html(""); // table is emptied before being filled
+    d3.select("#thead").html("");
+    d3.select("#tbody").html("");
+        // table is emptied before being filled
 
     var table_head = document.getElementById("thead");
     var h_row = document.createElement("tr");
-    table_head.appendChild(setTableRows(h_row, "th", keys))
+    table_head.appendChild(setTableRows(h_row, "th", keys)) 
+        // adds a completed row to table head
 
     var table_body = document.getElementById("tbody");
     data.forEach(function(d) { 
+        // for each row within the data, a row is created and added to body
         var b_row = document.createElement("tr");
         table_body.appendChild(setTableRows(b_row, "td", d));
     })
