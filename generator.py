92012c96ae13e14ad077811aaee502461bbbcbc0
Adrian Logan <alogan000@citymail.cuny.edu>
2016-07-06 12:26:22 -0400

    Help Menu further updates

--- generator.py
+++ generator.py
@@ -324,21 +324,21 @@ def manage_csv_folder(repo_dir, repo, from_date, to_date):
 	
 	os.chdir(csv_path) # switch to csv folder of chosen repo
 	print("2: " + os.getcwd())
-	# create_log(repo, from_date, to_date, repo_address) # make logfile
+	create_log(repo, from_date, to_date, repo_address) # make logfile
 	
-	# create_complexity_files(repo, repo_address, from_date, to_date)
-	# os.chdir(csv_path)
+	create_complexity_files(repo, repo_address, from_date, to_date)
+	os.chdir(csv_path)
 	
-	# generate_data_summary(repo, from_date, to_date)
-	# generate_data_metrics(repo, from_date, to_date)
-	# generate_data_coupling(repo, from_date, to_date)
-	# generate_data_age(repo, from_date, to_date)
-	# generate_data_hotspots(repo, from_date, to_date)
+	generate_data_summary(repo, from_date, to_date)
+	generate_data_metrics(repo, from_date, to_date)
+	generate_data_coupling(repo, from_date, to_date)
+	generate_data_age(repo, from_date, to_date)
+	generate_data_hotspots(repo, from_date, to_date)
 
 	os.chdir(settings.v3_dir)
 	# print("3: " + os.getcwd())
 	# flash('Analysis complete.')
-	# return csv_path
+	return csv_path
 
 
 # called by parse_csv
