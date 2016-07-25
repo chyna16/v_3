d34ef8f621638cd3f5d0bcad51e31074934f4f4a
Farhat Nader <farhat.nader01@myhunter.cuny.edu>
2016-07-24 22:43:15 -0400

    wrote more tests

--- tests/stash_api_test.py
+++ tests/stash_api_test.py
@@ -1,28 +1,57 @@
 import sys
 sys.path.append('..')
 import settings
-import os
-import csv
-from flask import Flask
+import stash_api
+import requests
+from requests.auth import HTTPBasicAuth
+from json import load
+import json
+import codecs
+import datetime
 import unittest
-import tempfile
-import shutil
-from nltk.stem.lancaster import LancasterStemmer
-from stop_words import get_stop_words
-import generator
 
 
 class ApiCallTests(unittest.TestCase):
 	def setUp(self):
 		pass
-		# self.url_projects = 'https://stash.mtvi.com/rest/api/1.0/projects?limit=100'
-		# self.projects = requests.get(
-		# 	url=url_projects, auth=(settings.username, settings.password)
-		# )	# retrieves data from api call using given username & password
-
-		# # reader = codecs.getreader("utf-8")
-		# self.json_projects = json.loads(projects.text)
-		# 	# converts retrieved data into a readable list of dictionaries
 
 	def tearDown(self):
 		pass
+
+	def test_get_repo_url_http(self):
+		url = stash_api.get_repo_url('arc-ui', 'http')
+		self.assertIn('arc-ui.git', url)
+		self.assertIn('http', url)
+
+	def test_get_repo_url_ssh(self):
+		url = stash_api.get_repo_url('arc-ui', 'ssh')
+		self.assertIn('arc-ui.git', url)
+		self.assertIn('ssh', url)
+
+	def test_get_repo_timestamp_date(self):
+		timestamp = stash_api.get_repo_timestamp('bot', 'mcshake', '1')[0].split(' ')
+		self.assertIs(2, len(timestamp))
+		date = timestamp[0].split('-')
+		self.assertIs(3, len(date))
+
+	def test_get_repo_timestamp_time(self):
+		timestamp = stash_api.get_repo_timestamp('bot', 'mcshake', '1')[0].split(' ')
+		self.assertIs(2, len(timestamp))
+		time = timestamp[1].split(':')
+		self.assertIs(3, len(time))
+
+	def test_get_project_repos(self):
+		repos = stash_api.get_project_repos('BOT', 'http', '100')
+		self.assertIn('stash.mtvi.com/scm/bot', repos[0]['url'])
+
+	def test_get_repo_detail(self):
+		key = stash_api.get_repo_detail('arc-ui', 'project', 'key')
+		self.assertEqual('ARC', key)
+
+	def test_get_projects(self):
+		projects = stash_api.get_projects()
+		self.assertIn(('AO', 'Always On'), projects)
+
+
+if __name__ == '__main__':
+	unittest.main()
