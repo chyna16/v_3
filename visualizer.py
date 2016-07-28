581ff5d9359f7623b25172adfbcc5129466d7669
Farhat Nader <farhat.nader01@myhunter.cuny.edu>
2016-07-28 12:41:12 -0400

    added error handling for invalid url query

--- visualizer.py
+++ visualizer.py
@@ -2,7 +2,7 @@ import os
 import fnmatch
 import json
 import io
-from flask import Flask, request, render_template, redirect, url_for, flash, jsonify
+from flask import Flask, request, render_template, redirect, url_for, flash, jsonify, abort
 import generator # our script
 import data_manager # our script
 import repo_manager
@@ -86,7 +86,7 @@ def return_repos():
 def return_repo():
 	key = request.args.get('key', '', type=str)
 	name = request.args.get('name', '', type=str)
-	if not key == '' or name == '':
+	if key == '' or name == '':
 		return jsonify(null='api call did not return anything')
 	dates = stash_api.get_repo_timestamp(key, name, '15000')
 	return jsonify(result=dates)
@@ -147,6 +147,8 @@ def result():
 
 	if request.method == 'GET':
 		analysis = request.args.get('analysis')
+		if not repo_name or not analysis or not from_date or not to_date:
+			abort(404)
 		repo_details = repo_name + "_" + from_date + "_" + to_date
 		if analysis == "cloud":
 			data, keys = get_log_data(os.path.join(csv_dir, repo_details), 
@@ -155,7 +157,7 @@ def result():
 			data, keys = get_csv_data(os.path.join(csv_dir, repo_details), 
 				analysis + "_" + repo_name + ".csv")
 		if data == []: 
-			return render_template('404.html')
+			abort(404)
 		return render_template('result.html',
 			repo_name=json.dumps(repo_name), analysis=json.dumps(analysis),
 			from_date=from_date, to_date=to_date,
