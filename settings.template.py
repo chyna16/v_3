2428c6bc164b7f0b65983711e99901f47ac6e71b
Adrian Logan <alogan000@citymail.cuny.edu>
2016-08-09 11:38:53 -0400

    Added settings template and requirements.txt

---
+++ settings.template.py
@@ -0,0 +1,15 @@
+maat_dir='/{path}/ixmaat0.8.5'  # path to codemaat
+repo_dir='/{path}/repos/'  # Path to save cloned repos
+csv_dir='/{path}/csv/'  # Path to CSV folder
+v3_dir='/{path}/v3/'  # Path to V3
+
+# Celery Settings
+BROKER_URL = 'amqp://guest:guest@localhost//'
+RESULT_BACKEND = 'rpc://'
+csvcat_path = 'csvcat'
+
+DEBUG_MODE = True
+
+# Stash Credentials
+username =
+password =
